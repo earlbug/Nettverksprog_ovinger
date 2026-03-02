56a48b19f74f69b8fb90ac2938def364cfa735c8
earlbug <erlendsundsdal@gmail.com>
2026-03-02 09:42:03 +0100

    add P4

---
+++ P5/P5_frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Coderunner</h1>
+  <p>
+    A website where you can write [...] code, run it on a serverside server, and get the output in return.
+  </p>
+
+  <h2>Insert code to be compiled and rendered serverside here:</h2>
+  <textarea v-model="inputCode"></textarea>
+
+  <h2>Output:</h2>
+  <output v-bind>output</output>
+  
+
+
+</template>
+
+<style scoped></style>
